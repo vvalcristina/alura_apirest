b1be2784a93e72a31d2bee3fd375ba74ac07da6a
valeria <silvvavaleria@gmail.com>
2020-03-29 13:45:42 -0300

    Incluindo paginação e ordenação default

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+/*EnableSpringDataWebSupport: flexibiliza a paginacao e ordenacao*/
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -35,10 +37,8 @@ public class TopicosController {
     //Quando se usa paginação o retorna Page
     @GetMapping
     public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso,
-                                 @RequestParam int pagina, int quantidade){
-
-        Pageable paginacao = PageRequest.of(pagina, quantidade);
-
+                                 @PageableDefault(sort="id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao){
+    //PageableDefault: vem quando não se passa parâmetros de ordencao
         if(nomeCurso == null){
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDTO.converter(topicos);
